fabd7fd8abfc7b082935ce3ee19a7aa0a275b2fe
Andrei <andrei.nakarchuk@gmail.com>
2024-11-19 16:24:13 +0300

    Creating a store, todo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import  { TaskType, Todo } from './Components/TodoList/Todo';
+import { v1 } from 'uuid';
+
+
+type FilterValuesType = 'all' | 'active' | 'completed'
+
+type TodoListType = {
+    id: string
+    title: string
+    filter: FilterValuesType
+}
+
+
+
+
 
 function App() {
+
+  const [taskList, setTaskList] = useState<TaskType[]>([
+    {id: v1(), title: 'test1', isDone: true},
+    {id: v1(), title: 'test2', isDone: false},
+    {id: v1(), title: 'test3', isDone: true}
+  ])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     work
+     <Todo tasks={taskList} />
     </div>
   );
 }
---
+++ src/store/store.ts
@@ -0,0 +1,13 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+
+
+const RootReducer = combineReducers({
+
+})
+
+export const store = configureStore({
+    reducer: RootReducer
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
